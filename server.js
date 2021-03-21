9496eaef29d9ef7a1b58dc42879f22381d4ed296
luxxm <robloxvr21@gmail.com>
2021-03-21 18:14:27 +0100

    implement api

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
+const fetch = require('node-fetch');
 
 //start app
 var app = express();
+const WEBHOOK_URL = process.env.WEBHOOK_URL;
+
+//      HANDLE WEBSITE
 
 //serve index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/build/index.html'));
 });
+
+//      HANDLE API
+
+app.post('/api/send_message', (req, res) => {
+    fetch(WEBHOOK_URL, {
+        method: "post",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: `${req.query.name} - ${req.query.email}`,
+            content: `${req.query.message}`,
+        })
+    });
+
+    res.end();
+})
 
 //serve static content
 app.use(express.static(path.join(__dirname, 'build')))
